84eb38fcd64bb10b0b04f9bedfda384acc3f7bbf
aniketdarp190301 <darpaniket10@gmail.com>
2022-04-30 12:38:19 +0530

    5

--- Assignment 3a/app.js
+++ Assignment 3a/app.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
 // Static Middleware
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // View Engine Setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get("/", function (req, res) {
-  res.render("index.ejs");
+app.get('/', function (req, res) {
+  res.render('index.ejs');
 });
 
-app.listen(3000, () => console.log(`Server started at port : 3000`));
+app.listen(5000, () => console.log(`Server started at port : 5000`));
